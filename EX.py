224f217b63c1fde2b1b08b81b67c3af48e5bf6da
Mr. Ex <103701389+EX-OFFICIAL@users.noreply.github.com>
2023-11-17 23:23:28 +0600

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,7 @@
+import os, platform
+print('\033[38;5;76mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
+print('\033[38;5;119mEX IS REAL\n\033[1;92mI AM EX OF NATIONS')
+os.system("git pull")
+os.system("xdg-open https://www.facebook.com/EX.OFFICI4L")
+import sex
+sex.login()
---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Installation
+
+
+
+- `rm -rf EX-FUCK`
+
+- `git clone https://github.com/EX-OFFICIAL/EX-FUCK`
+
+- `cd EX-FUCK`
+
+- `chmod +x *`
+
+- `python EX.py`
+
